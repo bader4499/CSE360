66565b6cd00a83f9b123178a7c3056c9c91753a7
bader <bader@192.168.0.6>
2020-02-27 14:54:34 -0700

    Update add(int) and remove(int)
    add
    	- append(int)
    	- int size()
    	- int first()
    	- int last()

--- cse360assign2/src/SimpleList.java
+++ cse360assign2/src/SimpleList.java
@@ -19,7 +19,12 @@ public class SimpleList {
 	
 	// add an integer to the list
 	public void add(int integer) {
-		
+		if(count == list.length) {
+			int[] list2 = new int[(int) (list.length*1.5)];
+			for(int i = 0; i < count; i++)
+				list2[i] = list[i];
+			list = list2;
+		}
 		for(int i = count-1; i >= 0; i--) {
 			if(count != 10) {
 				list[i+1] = list[i]; // move integer
@@ -30,7 +35,12 @@ public class SimpleList {
 	}
 	// remove an integer from the list
 	public void remove(int integer) {
-		
+		if((count/list.length) >= 0.25 && list.length != 1) {
+			int[] list2 = new int[list.length-1];
+			for(int i = 0; i < count; i++)
+				list2[i] = list[i];
+			list = list2;
+		}
 		int index = search(integer); // store index of integer
 		
 		if(index != -1) {
@@ -39,6 +49,41 @@ public class SimpleList {
 			}
 		}
 		count--; // decrement count
+	}
+	
+	//The append method should append the parameter to the end of the list.  If the list was full, then increase the size by 50% so there will be room.  Increment the count.
+	public void append (int integer) {
+		if(count == list.length) {
+			int[] list2 = new int[(int) (list.length*1.5)];
+			for(int i = 0; i < count; i++)
+				list2[i] = list[i];
+			list = list2;
+		}
+		list[count] = integer;
+		count++;
+	}
+	
+	// Return the current number of possible locations in the list
+	public int size() {
+		return list.length;
+	}
+	
+	// Return the first element in the list.  If there are no elements n the list, then return -1.
+	public int first() {
+		int element = -1;
+		if(count != 0) {
+			element = list[0];
+		}
+		return element;
+	}
+	
+	// Return the last element in the list.  If there are no elements n the list, then return -1.
+	public int last() {
+		int element = -1;
+		if(count != 0) {
+			element = list[count-1];
+		}
+		return element;
 	}
 	
 	// return count of integers in the list
